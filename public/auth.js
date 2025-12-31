45ac166dd0f39db9ee742f65f807a7e6c4017b34
sopheakneath1710-pixel <sopheakneath1710@gmail.com>
2025-12-31 12:38:20 +0700

    Update auth.js

--- public/auth.js
+++ public/auth.js
@@ -1,7 +1,7 @@
 // =====================
 // SUPABASE CONFIG
 // =====================
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
+const SUPABASE_URL = "https://vobglhlucwodmlzphmvc.supabase.co";
 const SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY";
 
 const supabase = window.supabase.createClient(
